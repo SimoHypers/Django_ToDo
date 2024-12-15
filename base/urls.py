49ff2f42f85f608ac4830b78b276c903931bd507
Simo <153776287+SimoHypers@users.noreply.github.com>
2024-12-15 16:20:02 +0400

    Finished all logic to the application

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, taskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, CustomLogoutView
+from .views import TaskList, taskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, CustomLogoutView, RegisterPage
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
     
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', taskDetail.as_view(), name='tasks'),
